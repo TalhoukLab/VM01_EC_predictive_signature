ecee4883bb075b9e8b38daab747d35ed697f89b8
ddodani <ddodani@student.ubc.ca>
2023-09-05 08:38:03 -0700

    DET results

--- 01-Reproducibility_Replicability/RScripts/01-Analysis/lefse_dataPrep.R
+++ 01-Reproducibility_Replicability/RScripts/01-Analysis/lefse_dataPrep.R
@@ -1,42 +1,51 @@
 ## This script will format the input for Lefse
-pipeline <- "Tsementzi"
-levels <- c("OTU")
+pipeline <- "Chao"
+levels <- c("phylum", "class", "order", "family", "genus")
 cohorts <- c("Antonio","Chao", "Gressel", "Tsementzi", "Walsh")
 for(cohort in cohorts){
   for(level in levels){
-    ## OTU level
-    if(level=="OTU"){
-      rel_phyloseq <- microbiome::transform(eval(parse(text=paste0(cohort, "_", pipeline, "phyloseq_tree_raw"))), "compositional")
-      data <- data.frame(otu_table(rel_phyloseq))
-      taxa_names <- data.frame(tax_table(rel_phyloseq))
-      taxa_names$merged <- paste(rownames(taxa_names), taxa_names$kingdom, taxa_names$phylum, taxa_names$class,
-                                 taxa_names$order, taxa_names$family, taxa_names$genus, sep = "|")
-      rownames(data) <- taxa_names$merged
-    } else {
-      agg_taxa <- aggregate_taxa(eval(parse(text = paste0(cohort, "_", pipeline, "phyloseq_tree_raw"))), level = level)
-      data <- data.frame(otu_table(agg_taxa))
-      taxa_names <- data.frame(tax_table(agg_taxa))
-      rownames(data) <- taxa_names$unique
-    }
+    agg_taxa  <- subset_taxa(eval(parse(text = paste0(cohort, "_", pipeline, 'phyloseq_tree_raw'))), 
+                                c(eval(parse(text=level))!=" " & eval(parse(text=level)) !=""))
+    agg_taxa <- aggregate_taxa(agg_taxa, level = level)
+    data <- data.frame(otu_table(agg_taxa))
+    taxa_names <- data.frame(agg_taxa@tax_table)
+    rownames(data) <- taxa_names$unique
     flip_data <- data.frame(t(data), check.names = F)
-    if(cohort=="Antonio"| cohort=="Walsh"){
+    if(cohort=="Antonio"){
       FT <- as.data.frame(sample_data(eval(parse(text = paste0(cohort, "_", pipeline, "phyloseq_tree_raw")))))
-      #FT <- data.frame(FT) %>% 
-      #  mutate(Obesity = if_else(BMI >=30, "Obese", "Non-obese"))
-      FT$pH <- as.numeric(FT$pH)
+      FT$age <- as.numeric(FT$age)
       FT$BMI <- as.numeric(FT$BMI)
-      covariates = FT[, c('Age', 'histology', 'pH', 'BMI', 'sraID')]
+      FT$pHRecoded <- as.numeric(as.factor(FT$pHRecoded))
+      FT$menopausal.status <- as.numeric(as.factor(FT$menopausal.status))
+      covariates = FT[, c('histology', 'sraID', 'pHRecoded', 'BMI', 'age', 'menopausal.status')]
       data_full <- cbind(covariates, flip_data)
-    } else if ( cohort=="Tsementzi" ){
+    }
+    if(cohort=="Walsh"){
       FT <- as.data.frame(sample_data(eval(parse(text = paste0(cohort, "_", pipeline, "phyloseq_tree_raw")))))
-      #FT <- data.frame(FT) %>% 
-      # mutate(Obesity = if_else(BMI >=30, "Obese", "Non-obese"))
-      FT$pH <- as.numeric(FT$pH)
+      FT$age <- as.numeric(FT$age)
       FT$BMI <- as.numeric(FT$BMI)
-      covariates = FT[, c('age', 'histology', 'pH', 'BMI', 'sraID')]
-      #covariates = FT[, c('histology', 'sraID')]
+      FT$pHRecoded <- as.numeric(as.factor(FT$pHRecoded))
+      FT$menopausal.status <- as.numeric(as.factor(FT$menopausal.status))
+      FT$ethnicityRecoded <- as.numeric(as.factor(FT$ethnicityRecoded))
+      covariates = FT[, c('histology', 'sraID', 'age','pHRecoded', 'BMI','menopausal.status', 'ethnicityRecoded' )]
       data_full <- cbind(covariates, flip_data)
-    } else{
+    } 
+    if(cohort == "Tsementzi" ){
+      FT <- as.data.frame(sample_data(eval(parse(text = paste0(cohort, "_", pipeline, "phyloseq_tree_raw")))))
+      FT$age <- as.numeric(FT$age)
+      FT$BMI <- as.numeric(FT$BMI)
+      FT$pHRecoded <- as.numeric(as.factor(FT$pHRecoded))
+      FT$ethnicityRecode <- as.numeric(as.factor(FT$ethnicityRecode))
+      covariates = FT[, c('histology','sraID', 'age','pHRecoded', 'BMI', 'ethnicityRecode')]
+      data_full <- cbind(covariates, flip_data)
+    } 
+    if(cohort == "Chao"){
+      FT <- as.data.frame(sample_data(eval(parse(text = paste0(cohort, "_", pipeline, "phyloseq_tree_raw")))))
+      covariates = FT[, c('histology', 'sraID', 'age')]
+      FT$age <- as.numeric(FT$age)
+      data_full <- cbind(covariates, flip_data)
+    }
+    if(cohort == "Gressel"){
       FT <- as.data.frame(sample_data(eval(parse(text = paste0(cohort, "_", pipeline, "phyloseq_tree_raw")))))
       covariates = FT[, c('histology', 'sraID')]
       data_full <- cbind(covariates, flip_data)
